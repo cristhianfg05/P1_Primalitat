4f543509e86a610557aa50ca86d013d91300dc3d
Cristhian.cfg <cristhianfg.cfg@gmail.com>
2021-10-14 21:07:00 +0200

    Primeros Tests

--- src/Main_P1.java
+++ src/Main_P1.java
@@ -2,12 +2,7 @@
 public class Main_P1 {
 
 	public static void main(String[] args) {
-		System.out.print("GOLA GOLA");
 		
-		
-		System.out.print("GOLA GOLA");
-		System.out.print("GOLA GOLA");
-		System.out.print("GOLA GOLA");
 
 	}
 
