1ed46580bbf4a1d543ff03f93db193a2667ac6c9
nagu10 <nahuelsw99@hotmail.com>
2021-10-28 15:49:53 +0200

    Prueba de cambio
    Es una prueba nada mas

--- src/Main_P1.java
+++ src/Main_P1.java
@@ -10,7 +10,7 @@ public class Main_P1 {
 			System.out.print(TestsPrimalitat.esPrimoHastaP(999983)+"\n");
 			System.out.print(TestsPrimalitat.esPrimoHasta_P_Partido2(999983)+"\n");
 			System.out.print(TestsPrimalitat.esPrimoHastaP_BigInteger(new BigInteger("999994"))+"\n");
-
+			//El cristhian es buena gente 
 		
 	}
 
